a8662cb3e95b26a5a30022c21223a2008f9a646e
JeffreyZou13 <jeffreyzou13@gmail.com>
2014-11-26 10:13:10 -0500

    Update WordSearch.java

--- WordSearch.java
+++ WordSearch.java
@@ -1,3 +1,15 @@
 public class WordSearch {
-
+  char[][] wordGrid;
+  ArrayList <String> hidden;
+  ArrayList <String> possible; //Optional
+  
+  //Default Constructor
+  public WordSearch {
+    //Sets the grid to 10x10
+    wordGrid = new char[10][10];
+  }
+  
+  public WordSearch (int rows, int cols) {
+    wordGrid = new char[rows][cols];
+  }
 }
